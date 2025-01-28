b9ec5de7ea9fadbeba20eae2e239c4dbe66d9717
Ufuk Özsahin <oezsahin95@icloud.com>
2025-01-28 17:13:54 +0100

    building optimization

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,25 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Router } from '@angular/router'; 
+import { RouterModule, Router } from '@angular/router';
+import { HeaderComponent } from "../shared/header/header.component";
+import { LogInComponent } from "./log-in/log-in.component"; 
 // import { GoogleAuthProvider, signInWithRedirect, Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, LogInComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
-  focusedEmail = false;
-  focusedPassword = false;
 
-  // constructor(private router: Router, private afAuth: Auth) {}
-
-
-
-  // googleLogin(){
-  //   signInWithRedirect(this.afAuth, new GoogleAuthProvider());
-  // }
 }
---
+++ src/app/landing-page/log-in/log-in.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+// import { GoogleAuthProvider, signInWithRedirect, Auth } from '@angular/fire/auth';
+
+
+
+@Component({
+  selector: 'app-log-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './log-in.component.html',
+  styleUrl: './log-in.component.scss'
+})
+export class LogInComponent {
+  focusedEmail = false;
+  focusedPassword = false;
+
+  // constructor(private router: Router, private afAuth: Auth) {}
+
+
+
+  // googleLogin(){
+  //   signInWithRedirect(this.afAuth, new GoogleAuthProvider());
+  // }
+}
