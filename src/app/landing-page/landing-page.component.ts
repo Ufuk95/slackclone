c2280edd5d012fe25eb2517d34734c45a1360e81
Ufuk Özsahin <oezsahin95@icloud.com>
2025-02-03 11:38:31 +0100

    implementing createAccount component and finishing login page optic

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,16 +3,25 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 import { HeaderComponent } from "../shared/header/header.component";
-import { LogInComponent } from "./log-in/log-in.component"; 
+import { LogInComponent } from "./log-in/log-in.component";
+import { FooterComponent } from "../shared/footer/footer.component";
+import { CreateAccountComponent } from "./create-account/create-account.component"; 
 // import { GoogleAuthProvider, signInWithRedirect, Auth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent, LogInComponent],
+  imports: [HeaderComponent, LogInComponent, FooterComponent, CreateAccountComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+  isLogInVisible: boolean = true;
+  isCreateAccountVisible: boolean = false;
 
+
+  onCreateAccount() {
+    this.isLogInVisible = false;
+    this.isCreateAccountVisible = true;
+  }
 }
