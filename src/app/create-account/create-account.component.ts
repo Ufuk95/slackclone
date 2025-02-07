49b1e2c28a7bcec7e5df1ac7c4994e62b89311fb
Ufuk Özsahin <oezsahin95@icloud.com>
2025-02-07 12:24:26 +0100

    optical adjustment of createAcc. component and preparation for the next component

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -2,18 +2,48 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../shared/footer/footer.component";
 import { HeaderLogoComponent } from "../shared/header-logo/header-logo.component";
 import { RouterModule } from '@angular/router';
+import { ChooseAvatarComponent } from "./choose-avatar/choose-avatar.component";
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [FooterComponent, HeaderLogoComponent, RouterModule],
+  imports: [FooterComponent, HeaderLogoComponent, RouterModule, ChooseAvatarComponent],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
 
+  isCheckboxHovered: boolean = false;
+  isCheckboxChecked: boolean = false;
+  isArrowbackHovered: boolean = false;
+  isLoginDataRight: boolean = false;
+  isCreateAccountNotFinished: boolean = true;
 
   onSubmit() {
-    
+
+  }
+
+  get CheckboxImage(): string {
+    if (this.isCheckboxChecked) {
+      return this.isCheckboxHovered
+        ? '/img/landingPage/2.kontoErstellen/hover_checked.svg'
+        : '/img/landingPage/2.kontoErstellen/checked_box.svg';
+    } else {
+      return this.isCheckboxHovered
+        ? '/img/landingPage/2.kontoErstellen/hover_empty_checkbox.svg'
+        : '/img/landingPage/2.kontoErstellen/empty_checkbox.svg';
+    }
+  }
+
+  toggleCheckbox(): void {
+    this.isCheckboxChecked = !this.isCheckboxChecked;
+  }
+
+  get hoverArrowbackSrc(): string {
+    if (this.isArrowbackHovered) {
+      return '/img/landingPage/2.kontoErstellen/blue_arrow_back.svg';
+    } else {
+      return '/img/landingPage/2.kontoErstellen/arrow_back.svg'
+    }
   }
 }
